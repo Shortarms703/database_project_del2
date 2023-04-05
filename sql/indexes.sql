c430c09aa4e6fa077c72c2a5d0812b5d9cf66b90
Elsa Ko <eko095@uottawa.ca>
2023-04-04 23:07:00 -0400

    Preliminary Indexes for Review

---
+++ sql/indexes.sql
@@ -0,0 +1,6 @@
+-- unsure about indexes as we only just saw them in lecture recently
+CREATE UNIQUE INDEX login
+ON Employee (first_name, password);
+
+CREATE UNIQUE INDEX rooms
+ON Room (room_num);
